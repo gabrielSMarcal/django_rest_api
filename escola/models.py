8c21934674e37fd39ccf384014306abefdbc7cd4
yhamerith <gabriel.santos.marcar1@gmail.com>
2025-07-07 17:59:52 -0300

    Adiciona modelos Estudante e Curso com campos e migrações iniciais

--- escola/models.py
+++ escola/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Estudante(models.Model):
+    
+    nome = models.CharField(max_length = 100)
+    email = models.EmailField(blank = False, max_length = 40)
+    cpf = models.CharField(max_length = 11)
+    data_nascimento = models.DateField()
+    celular = models.CharField(max_length = 14)
+    
+    def __str__(self):
+        return self.nome
+
+class Curso(models.Model):
+    
+    NIVEL = (
+        ('B', 'Básico'),
+        ('I', 'Intermediário'),
+        ('A', 'Avançado'),
+    )
+    
+    codigo = models.CharField(max_length = 10)
+    descricao = models.CharField(max_length = 100, blank = False)
+    nivel = models.CharField(max_length = 100, choices = NIVEL, blank = False, null = False, default = 'B')
+    
+    def __str__(self):
+        return f'{self.codigo} - {self.descricao} ({self.get_nivel_display()})'
+    
