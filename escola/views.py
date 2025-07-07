97ff7f8803a3088827f7c475bee3753adbb446e6
yhamerith <gabriel.santos.marcar1@gmail.com>
2025-07-07 17:32:43 -0300

    first commit

---
+++ escola/views.py
@@ -0,0 +1,10 @@
+from django.http import JsonResponse
+
+def estudantes(request):
+    if request.method == 'GET':
+        estudantes = {
+            'id': '1',
+            'nome': 'Gabriel'
+        }
+        
+    return JsonResponse(estudantes)
