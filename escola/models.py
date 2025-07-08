b33f00e62b58a2884f769ccfe38cb7ed1b1c1bbd
yhamerith <gabriel.santos.marcar1@gmail.com>
2025-07-08 17:52:38 -0300

    Adiciona modelo Matricula com relacionamentos e campo de período

--- escola/models.py
+++ escola/models.py
@@ -26,3 +26,24 @@ class Curso(models.Model):
     def __str__(self):
         return f'{self.codigo} - {self.descricao} ({self.get_nivel_display()})'
     
+class Matricula(models.Model):
+    
+    PERIODO = (
+        ('M', 'Matutino'),
+        ('V', 'Vespertino'),
+        ('N', 'Noturno'),
+    )
+    
+    estudante = models.ForeignKey(Estudante, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    periodo = models.CharField(max_length = 100, choices = PERIODO, blank = False, null = False, default = 'M')
+
+'''Id
+Relacionar com o modelo Estudante
+Se o Estudante for deletado, todas as matrículas daquele estudante devem ser deletadas
+Relacionar com o modelo Curso
+Se o Curso for deletado, todas as matriculas daquele curso devem ser deletadas
+Período (Matutino, Vespertino, Noturno)
+Não pode estar em Branco
+Não pode ser Nulo
+Por padrão deve ser Matutino'''
