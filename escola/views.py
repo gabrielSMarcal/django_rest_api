234b5dd13941a0d808db2e74a231d0f729a4e518
yhamerith <gabriel.santos.marcar1@gmail.com>
2025-07-08 16:50:01 -0300

    Remove a função de estudantes e adiciona viewsets para Estudante e Curso

--- escola/views.py
+++ escola/views.py
@@ -1,10 +1,13 @@
-from django.http import JsonResponse
+from escola.models import Estudante, Curso
+from escola.serializers import EstudanteSerializer, CursoSerializer
+from rest_framework import viewsets
 
-def estudantes(request):
-    if request.method == 'GET':
-        estudantes = {
-            'id': '1',
-            'nome': 'Gabriel'
-        }
-        
-    return JsonResponse(estudantes)
+class EstudanteViewSet(viewsets.ModelViewSet):
+    
+    queryset = Estudante.objects.all()
+    serializer_class = EstudanteSerializer
+    
+class CursoViewSet(viewsets.ModelViewSet):
+    
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
