471cce3ceeeb0a86f7e67e7f0ecece1a54a142d9
yhamerith <gabriel.santos.marcar1@gmail.com>
2025-07-11 17:21:59 -0300

    Adiciona unicidade ao campo 'cpf' no modelo Estudante

--- escola/models.py
+++ escola/models.py
@@ -4,7 +4,7 @@ class Estudante(models.Model):
     
     nome = models.CharField(max_length = 100)
     email = models.EmailField(blank = False, max_length = 40)
-    cpf = models.CharField(max_length = 11)
+    cpf = models.CharField(max_length = 11, unique=True)
     data_nascimento = models.DateField()
     celular = models.CharField(max_length = 14)
     
